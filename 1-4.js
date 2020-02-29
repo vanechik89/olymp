a8772865cb790dcc8675902e4ce10da10f125f6c
vanechik89 <61108506+vanechik89@users.noreply.github.com>
2020-02-29 20:07:11 +0300

    Update 1-4.js

--- 1-4.js
+++ 1-4.js
@@ -8,7 +8,7 @@
   GM_setValue('dynLevelCoef_BTCUSD', 1.82)
   GM_setValue('dynLevelCoef_ETHUSD', 1.85)
   GM_setValue('dynLevelCoef_XRPUSD', 1.86)
-  GM_setValue('dynLevelCoef_ETCUSD', 1.8)
+  GM_setValue('dynLevelCoef_ETCUSD', 1.89)
   GM_setValue('dynLevelCoef_ZECUSD', 1.96)
   GM_setValue('dynLevelCoef_DASHUSD', 1.96)
 
