22dffe50bfe70f6e64e92e468436fa19db54ce2e
vanechik89 <61108506+vanechik89@users.noreply.github.com>
2020-04-19 13:49:39 +0300

    Update 1-4.js

--- 1-4.js
+++ 1-4.js
@@ -5,10 +5,10 @@
   GM_setValue('percentThreshold_ZECUSD', 80)
   GM_setValue('percentThreshold_DASHUSD', 80)
  
-  GM_setValue('dynLevelCoef_BTCUSD', 1.85)
-  GM_setValue('dynLevelCoef_ETHUSD', 1.88)
-  GM_setValue('dynLevelCoef_XRPUSD', 1.93)
-  GM_setValue('dynLevelCoef_ETCUSD', 1.88)
+  GM_setValue('dynLevelCoef_BTCUSD', 1.92)
+  GM_setValue('dynLevelCoef_ETHUSD', 1.9)
+  GM_setValue('dynLevelCoef_XRPUSD', 1.92)
+  GM_setValue('dynLevelCoef_ETCUSD', 1.9)
   GM_setValue('dynLevelCoef_ZECUSD', 1.96)
   GM_setValue('dynLevelCoef_DASHUSD', 1.96)
 
